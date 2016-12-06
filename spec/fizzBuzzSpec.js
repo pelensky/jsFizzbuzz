75327b93526969e62b79ea593b07fd0d6e77e513
keomony khun <keomony@gmail.com>
2016-12-06 15:43:45 +0000

    refactors

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,21 +1,43 @@
 describe('FizzBuzz', function() {
   var mony;
 
+  beforeEach(function(){
+    mony = new FizzBuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function (){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByThree(3)).toBe(true);
+      expect(mony.isDivisibleByThree(6)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByFive(5)).toBe(true);
+      expect(mony.isDivisibleByFive(10)).toBe(true);
     });
 
     it('divisible by 15', function(){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByFifteen(15)).toBe(true);
+      expect(mony.isDivisibleByFifteen(30)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is not', function () {
+    it('divisible by 3', function(){
+      expect(mony.isDivisibleByThree(1)).toBe(false);
+      expect(mony.isDivisibleByThree(4)).toBe(false);
+    });
+
+    it('divisible by 5', function(){
+      expect(mony.isDivisibleByFive(1)).toBe(false);
+      expect(mony.isDivisibleByFive(7)).toBe(false);
+    });
+
+    it('divisible by 15', function(){
+      expect(mony.isDivisibleByFifteen(1)).toBe(false);
+      expect(mony.isDivisibleByFifteen(9)).toBe(false);
     });
 
   });
