6f1f6334d4e528c69ba79cfed9deee73d8a620f8
Dan Pelensky <dan@pelensky.com>
2016-12-06 15:28:36 +0000

    refactored a before block

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,20 +1,21 @@
 describe('FizzBuzz', function() {
   var mony;
 
+  beforeEach(function(){
+    mony = new FizzBuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function (){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByThree(3)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by 15', function(){
-      mony = new FizzBuzz();
       expect(mony.isDivisibleByFifteen(15)).toBe(true);
     });
 
